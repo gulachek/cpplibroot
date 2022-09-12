24a3289a5964d2a8aaba2dec7c24924738665d4b
Nicholas Gulachek <nsgulachek@gmail.com>
2022-09-11 21:26:56 -0500

    Add base chocolatey package

---
+++ choco/tools/chocolateyUninstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+# Remove cpplibroot system directory
+$programFiles = $Env:ProgramFiles;
+$dir = "${programFiles}\cpplibroot";
+
+Remove-Item -Path $dir
+
+# Remove from CPP_LIBROOT_PATH
+$librootPath = $Env:CPP_LIBROOT_PATH
+$newVal = $librootPath.Replace($dir, "")
+$newVal = $newVal.Replace(";;", "")
+$newVal = $newVal -replace ";$", ""
+$newVal = $newVal -replace "^;", ""
+if (!$newVal) { $newVal = $null; }
+
+Install-ChocolateyEnvironmentVariable `
+-VariableName "CPP_LIBROOT_PATH" `
+-VariableValue $newVal `
+-VariableType Machine
---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$programFiles = $Env:ProgramFiles;
+$dir = "${programFiles}\cpplibroot";
+
+# Create folder
+New-Item -ItemType Directory -Path "${dir}"
+
+# Add to CPP_LIBROOT_PATH
+$librootPath = $Env:CPP_LIBROOT_PATH
+$newVal = "${dir};${librootPath}" 
+$newVal = $newVal -replace ";$", ""
+
+Install-ChocolateyEnvironmentVariable `
+-VariableName "CPP_LIBROOT_PATH" `
+-VariableValue $newVal `
+-VariableType Machine
